6c140c9eba2c980e08a04de7818eea8f69578133
孙辉 <sunhui@91160.com>
2022-10-17 15:56:38 +0800

    添加测试代码

---
+++ src/test/java/org/splendor/dependnew/test.java
@@ -0,0 +1,9 @@
+package org.splendor.dependnew;
+
+/**
+ * @author splendor.s
+ * @create 2022/10/17 下午3:55
+ * @description
+ */
+public class test {
+}
---
+++ src/test/java/org/splendor/seq/test.java
@@ -0,0 +1,9 @@
+package org.splendor.seq;
+
+/**
+ * @author splendor.s
+ * @create 2022/10/17 下午3:56
+ * @description
+ */
+public class test {
+}
